5ebd43e31d9608a5ec72de82cfe2250e8e890cad
satish shirahatti <satish.shirahatti@cognizant.com>
2020-06-10 15:02:42 +0200

    Updates

---
+++ precache-manifest.58b92fefd3806b63a2a4d01a94c58133.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4d95a87c7a02a69aacb",
+    "url": "/my-portfolio/static/js/runtime~main.d4d95a87.js"
+  },
+  {
+    "revision": "0f16b1547433b4527f07",
+    "url": "/my-portfolio/static/js/main.0f16b154.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/my-portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "0f16b1547433b4527f07",
+    "url": "/my-portfolio/static/css/main.9d3bb0cc.chunk.css"
+  },
+  {
+    "revision": "2e98d6520aa352e95f81248cf9786159",
+    "url": "/my-portfolio/index.html"
+  }
+];
